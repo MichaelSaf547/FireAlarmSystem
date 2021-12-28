7b7ce1fc67af363a8b558f235b450aa1d80fc97a
MichaelSaf547 <michaelsafwat547@gmail.com>
2021-12-28 13:24:35 +0200

    Restructure the code into three classes

--- GUI/src/gui/GUI.java
+++ GUI/src/gui/GUI.java
@@ -1,7 +1,14 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ * ITI INTAKE 42 EMBEDDED SYSTEM 
+ * JAVA COURSE 
+ * GROUP 8
+ * STUDENTS ARE 1-Michael Safwat Sobhy Nakhla --> "LEADER"
+ *              2-Abdelrahman Mahmoud Mohamed Saleh
+ *              3-Abdelrahman Omar Mohamed Shafik
+ *              4-Mostafa Hani Imam
+ *              5-mohamed maged abdrabuh 
  */
+
 package gui;
 
 import com.fazecast.jSerialComm.SerialPort;
@@ -9,28 +16,14 @@ import eu.hansolo.medusa.Gauge;
 import eu.hansolo.medusa.skins.FlatSkin;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Time;
-import java.time.LocalTime;
-import java.util.Optional;
-import java.util.Scanner;
-import java.util.Vector;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
@@ -47,165 +40,94 @@ import javafx.stage.StageStyle;
 
 public class GUI extends Application {
 
-    Gauge tempG = new Gauge();
-    Gauge humidG = new Gauge();
-    VBox vbox = new VBox();
-    Label label;
-    Label com_label;
-    HBox hGauge = new HBox();
-    HBox bButtons = new HBox();
-    Button test = new Button("Test");
-    Button stop = new Button("Stop");
-    Button log = new Button("Log");
-    Button start = new Button("Start");
+    // The whole comming are for the application Scene
+    Scene app_Scene;                        // Application Scene
+    Gauge tempG = new Gauge();              // Gauge for Temperature
+    Gauge humidG = new Gauge();             // Gauge for Humidity
+    VBox vbox = new VBox();                 // To handle the Application Scene
+    Label label;                            // Label for the name of the Arduino name and port                      
+    HBox hGauge = new HBox();               // For Humidity Gauge 
+    HBox bButtons = new HBox();             // For the four buttons in the Application Scene
+    
+    //The four buttons in the Applicatioin Scene
+    Button test = new Button("Test");       // To test the whole System     
+    Button stop = new Button("Stop");       // To stop the led and buzzer after a high temperature is detected 
+    Button log = new Button("Log");         // To open the Log Scene and see the Log data
+    Button start = new Button("Start");     // To start the system again after the stop is pressed 
+    
+    // Used serial port connection
     static SerialPort chosenPort;
-    PrintWriter output;
-    Scanner input;
-    int alertFlag = 0;
-    int temper;
-    int humid;
-    Scene scene;
+    
+    int alertFlag = 0;                      // Used as a flag to check if the temperature is higher than 26 C
+    
+    // The Whole comming is for the Log Scene
     Scene log_Scene;
-    Vector<Integer> data_H = new Vector<>();
-    Vector<Integer> data_T = new Vector<>();
-    Vector<String> time = new Vector<>();
 
-    int counter_Read = 0; // every 15 input will log one data
-    int counter_Log = 0; // to check how many data has benn logged
-
-    Label log_Label_DataT;
-    Label log_Label_DataH;
-    Label log_Label_Time;
-    Button log_Ok;
-    HBox log_HBox;
-    HBox log_Data_HBox;
-    VBox log_VBox;
-    TextArea log_AreaT;
-    TextArea log_AreaH;
-    TextArea log_AreaTD;
-    Group root2;
-    LocalTime currentTime;
-
-    // Start communication between Ardunio and Java using SerialPort
-    public String init_com() {
-        Vector<String> portList = new Vector<String>();
-        SerialPort[] portNames = SerialPort.getCommPorts();
-        String comPort = "";
-        for (int i = 0; i < portNames.length; i++) {
-            portList.add(portNames[i].getSystemPortName());
-            System.out.println(i + "- " + portNames[i].getSystemPortName());
-            comPort = portNames[i].getSystemPortName();
-        }
-        chosenPort = SerialPort.getCommPort(comPort);
-        chosenPort.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
-        output = new PrintWriter(chosenPort.getOutputStream());
-        input = new Scanner(chosenPort.getInputStream());
-        chosenPort.openPort();
-        return comPort;
-    }
-
-    // Send to Ardunio using SerialPort
-    public void send(String msg) {
-
-        Thread thread;
-        thread = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(100);
-                } catch (Exception e) {
-                }
-                output.println(msg);
-                output.flush();
-            }
-        };
-        thread.start();
-    }
-
-    // Recieve from Ardunio using SerialPort
-    public void recieve() {
-        Thread thread;
-        thread = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(100);
-                } catch (Exception e) {
-                }
-
-                while (input.hasNextLine()) {
-                    try {
-                        temper = Integer.parseInt(input.nextLine());
-                        humid = Integer.parseInt(input.nextLine());
-                        counter_Read++;
-                        if (counter_Read == 1) {
-                            data_T.add(temper);
-                            data_H.add(humid);
-                            currentTime = LocalTime.now();
-                            time.add(currentTime.getHour() + ":" + currentTime.getMinute() + ":"
-                                    + currentTime.getSecond());
-                            counter_Log++;
-                        } else if (counter_Read == 15) {
-                            counter_Read = 0;
-                        }
-                        if (counter_Log == 10) {
-                            data_T.remove(0);
-                            data_H.remove(0);
-                            time.remove(0);
-
-                            counter_Log--;
-                        }
-                        System.out.println(temper + " " + humid);
-                    } catch (Exception e) {
-                    }
-                }
-            }
-        };
-        thread.start();
-    }
+    Label log_Label_DataT;                  // For Temperature data    
+    Label log_Label_DataH;                  // For Humidity data
+    Label log_Label_Time;                   // For Time data
+    Button log_Ok;                          // To return to the Application Scene
+    HBox log_HBox;                          // To handle the labels 
+    HBox log_Data_HBox;                     // To handle the Data Areas 
+    VBox log_VBox;                          // To handle the Log Scene
+    TextArea log_AreaT;                     // For Temperature data
+    TextArea log_AreaH;                     // For Humidity data
+    TextArea log_AreaTD;                    // For Time data
+    
+    Group root2;                            // To handle the background image
+    Comm comm;                              // Comm object to start connection
+    
 
     @Override
     public void init() {
-        // label = new Label("FireAlarm Version 0.1");
-        label = new Label(" Ardunio Nano(" + init_com() + ")");
-        settempGauge(tempG);
-        sethumidGauge(humidG);
-        hGauge = new HBox(200, tempG, humidG);
-        hGauge.setAlignment(Pos.CENTER);
-        bButtons = new HBox(100, start, test, stop, log);
-        bButtons.setAlignment(Pos.CENTER);
-        label.setTextFill(Color.WHITE);
-        label.setFont(Font.font(26));
-        vbox.getChildren().add(bButtons);
-        vbox.getChildren().add(label);
-        test.setId("test");
-        stop.setId("stop");
-        log.setId("log");
-        start.setId("start");
+        
+        comm = new Comm();  // Start the communication
+        
+        label = new Label(" Ardunio Nano(" + comm.comPort + ")");       //Arduino name and connection port
+        
+        settempGauge(tempG);                                            // Set Temperature Gauge
+        sethumidGauge(humidG);                                          // Set Humidity Gauge
+        hGauge = new HBox(200, tempG, humidG);                          // Add gauges to Hbox
+        hGauge.setAlignment(Pos.CENTER);                                // Center the gauge
+        bButtons = new HBox(100, start, test, stop, log);               // Add four buttons to Hbox
+        bButtons.setAlignment(Pos.CENTER);                              // Center the buttons
+        label.setTextFill(Color.WHITE);                                 // Make the text white 
+        label.setFont(Font.font(26));                                   // Make the label font 26 
+        vbox.getChildren().add(bButtons);                               // Add buttons to Vbox
+        vbox.getChildren().add(label);                                  // Add label to Vbox
+        test.setId("test");                                             // Setid for the CSS file
+        stop.setId("stop");                                             // Setid for the CSS file
+        log.setId("log");                                               // Setid for the CSS file
+        start.setId("start");                                           // Setid for the CSS file
 
+        
+        //The whole comming are for the log
         Font font = Font.font("Verdana", FontWeight.BOLD, 18);
 
-        log_Label_DataT = new Label("          Temperature      ");
+        log_Label_DataT = new Label("          Temperature");
         log_Label_DataT.setFont(font);
         log_Label_DataH = new Label("         Humidity");
         log_Label_DataH.setFont(font);
-        log_Label_Time = new Label("           Time         ");
+        log_Label_Time = new Label("           Time");
         log_Label_Time.setFont(font);
-        log_Ok = new Button("Ok");
-        log_HBox = new HBox(log_Label_Time, log_Label_DataT, log_Label_DataH);
+        log_Ok = new Button("Ok");              
+        log_HBox = new HBox(log_Label_Time, log_Label_DataT, log_Label_DataH); // add labels
 
-        log_VBox = new VBox();
-
+        log_VBox = new VBox();                
+        
+        // Adjust Text area for Temperature 
         log_AreaT = new TextArea();
         log_AreaT.setPrefHeight(400);
         log_AreaT.setPrefWidth(300);
         log_AreaT.setEditable(false);
 
+        // Adjust Text area for Humidity
         log_AreaH = new TextArea();
         log_AreaH.setPrefHeight(400);
         log_AreaH.setPrefWidth(300);
         log_AreaH.setEditable(false);
 
+        // Adjust Text area for Time 
         log_AreaTD = new TextArea();
         log_AreaTD.setPrefHeight(400);
         log_AreaTD.setPrefWidth(300);
@@ -213,30 +135,28 @@ public class GUI extends Application {
 
         log_Data_HBox = new HBox();
 
-        log_Data_HBox.getChildren().addAll(log_AreaTD, log_AreaT, log_AreaH);
+        log_Data_HBox.getChildren().addAll(log_AreaTD, log_AreaT, log_AreaH); // add areas
 
-        log_VBox.getChildren().addAll(log_HBox, log_Data_HBox, log_Ok);
-        currentTime = LocalTime.now();
+        log_VBox.getChildren().addAll(log_HBox, log_Data_HBox, log_Ok); // add all parts in log Scene
 
     }
 
-    ////////////////////////////
+    // Print the Temp, Hum, and Time to the areas 
     public void print_Log_Data() {
         int i;
         log_AreaT.clear();
         log_AreaH.clear();
         log_AreaTD.clear();
-        for (i = 0; i < counter_Log; i++) {
-            log_AreaTD.appendText(time.get(i) + "\n");
-            log_AreaT.appendText(data_T.get(i) + "\n");
-            log_AreaH.appendText(data_H.get(i) + "\n");
+        for (i = 0; i < comm.get_Counter_Log(); i++) {
+            log_AreaTD.appendText(comm.time.get(i) + "\n");
+            log_AreaT.appendText(comm.data_T.get(i) + "\n");
+            log_AreaH.appendText(comm.data_H.get(i) + "\n");
         }
     }
 
     @Override
     public void start(Stage primaryStage) throws FileNotFoundException {
         StackPane root = new StackPane();
-        alertMethod();
         
         /* Set Scaling for The Buttons */
         test.setScaleX(1.25);
@@ -261,68 +181,72 @@ public class GUI extends Application {
         test.setDefaultButton(true);
 
         BorderPane Pane = new BorderPane();
+
+        // Read an image to be used as a background for Applicarion Scene
         Image image = new Image(new FileInputStream("1.jpg"));
         ImageView imageView = new ImageView(image);
         root2 = new Group(imageView);
-
+        
         imageView.setFitHeight(1000);
         imageView.setFitWidth(1500);
 
+        // The handling of Test key: just send "1" to Arduino to make it start led and buzzer
         test.setOnAction(e -> {
-            send("1");
+            comm.send("1");
         });
 
+        // The handling of Stop key: just send "2" to Arduino to make it stop led and buzzer
         stop.setOnAction(e -> {
-            send("2");
+            comm.send("2");
         });
 
+        // The handling of Start key: just send "3" to Arduino to make it start its funtion again
         start.setOnAction(e -> {
-            send("3");
+            comm.send("3");
             alertMethod();
         });
 
+        // The handling of Log key: Change the Scene from Applocation to Log
+        // And add the data to the areas
         log.setOnAction(e -> {
             primaryStage.setScene(log_Scene);
             print_Log_Data();
         });
 
+        // The handling of Log key: Change the Scene from Log to Applocation
         log_Ok.setOnAction(e -> {
-            primaryStage.setScene(scene);
+            primaryStage.setScene(app_Scene);
         });
 
-        recieve();
+        comm.recieve();             // Start recieving from Arduino Thread
 
+        alertMethod();              // Start the alert Thread 
+        
+        // Start a Thread to update the Gauges values
         new Thread(() -> {
             while (true) {
-                tempG.setValue(temper);
-                humidG.setValue(humid);
+                tempG.setValue(comm.temper);
+                humidG.setValue(comm.humid);
             }
         }).start();
 
-        
 
         Pane.setBottom(vbox);
         Pane.setCenter(hGauge);
         root.getChildren().add(root2);
         root.getChildren().add(Pane);
 
-        scene = new Scene(root, 1500, 600, Color.WHITE);
+        app_Scene = new Scene(root, 1500, 600, Color.WHITE);
         log_Scene = new Scene(log_VBox, 600, 600);
-        scene.getStylesheets().add(getClass().getResource("CSS.css").toString());
+        app_Scene.getStylesheets().add(getClass().getResource("CSS.css").toString());
 
         primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setTitle("FireAlarm");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(app_Scene);
         primaryStage.show();
-        /**
-         * if(true) { Alert alert = new Alert(AlertType.WARNING);
-         * alert.setTitle("Temprature Warning !");
-         * alert.setContentText("Temprature is above 50 °C");
-         * alert.showAndWait(); alert.set }
-         *
-         */
+        
     }
-
+    // Set the Temperature gauge data
     public void settempGauge(Gauge gauge) {
         gauge.setSkin(new FlatSkin(gauge));
         gauge.setDecimals(0);
@@ -338,7 +262,7 @@ public class GUI extends Application {
         gauge.setUnit("°C");
         gauge.setValue(0);
     }
-
+    // Set the Humidity gauge data
     public void sethumidGauge(Gauge gauge) {
         gauge.setSkin(new FlatSkin(gauge));
         gauge.setDecimals(0);
@@ -356,35 +280,25 @@ public class GUI extends Application {
         gauge.setValue(0);
     }
 
+    // Alert Thread appears when Temp >= 26
     public void alertMethod() {
         new Thread(() -> {
             while (alertFlag == 0) {
-                System.out.println("---------");
-                if (temper >= 26) {
+                
+                if (comm.temper >= 26) {
                     alertFlag = 1;
                 }
             }
             if (alertFlag == 1) {
-                Platform.runLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        Alert alert = new Alert(AlertType.WARNING);
-                        alert.setTitle("Temprature Warning !");
-                        alert.setContentText("Temprature is above or equal 26 °C");
-                        alert.show();
-                    }
+                Platform.runLater(() -> {
+                    Alert alert = new Alert(AlertType.WARNING);
+                    alert.setTitle("Temprature Warning !");
+                    alert.setContentText("Temprature is above or equal 26 °C");
+                    alert.show();
                 });
                 alertFlag =0;
             }
         }).start();
-    }
-
-    // public void CurrentTem
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
     }
 
 }
---
+++ GUI/src/gui/Proj_Main.java
@@ -0,0 +1,20 @@
+/*
+ * ITI INTAKE 42 EMBEDDED SYSTEM 
+ * JAVA COURSE 
+ * GROUP 8
+ * STUDENTS ARE 1-Michael Safwat Sobhy Nakhla --> "LEADER"
+ *              2-Abdelrahman Mahmoud Mohamed Saleh
+ *              3-Abdelrahman Omar Mohamed Shafik
+ *              4-Mostafa Hani Imam
+ *              5-mohamed maged abdrabuh 
+ */
+
+package gui;
+
+import static javafx.application.Application.launch;
+
+public class Proj_Main {
+    public static void main(String[] args) {
+        launch(GUI.class, args);
+    }
+}
